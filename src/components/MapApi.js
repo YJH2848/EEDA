d0e63ad020d7f1776ac361acd52fa9f5ba83a8c3
YJH2848 <yunj05763@gmail.com>
2022-11-18 14:32:10 +0900

    EEDA에서 받은 임시 데이터를 지도에 표시

--- src/components/MapApi.js
+++ src/components/MapApi.js
@@ -1,15 +1,28 @@
 import { useEffect } from "react";
 import * as S from "./style";
+import { markerdata } from "../MapData/data";
 
 const { kakao } = window;
 function MapApi() {
   useEffect(() => {
     const container = document.getElementById("map");
     const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      center: new kakao.maps.LatLng(35.241141, 128.899906),
       level: 3,
     };
     const map = new kakao.maps.Map(container, options);
+
+    markerdata.forEach(el => {
+      const m = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(el.lat, el.lng),
+      });
+      const mLabel = new kakao.maps.InfoWindow({
+        position: new kakao.maps.LatLng(el.lat, el.lng),
+        content: "언젠간 넣겠지",
+      });
+      mLabel.open(map, m);
+    });
   }, []);
 
   return (
